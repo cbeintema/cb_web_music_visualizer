3fe5cbb96ff41472611a7096bb9f7dc8e80bfe26
Chris Beintema <cbeintema@gmail.com>
2025-08-23 16:41:12 -0700

    adding proj for app and folder for web

---
+++ vizscrambler/vizscrambler/vizscramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  vizscramblerApp.swift
+//  vizscrambler
+//
+//  Created by Christopher R Beintema on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct vizscramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
